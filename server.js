d84fc8db21be10a5cecc859e20a9ca9700e76f67
Santiago Enciso <whereispolaris@gmail.com>
2019-10-12 18:30:43 -0500

    Installed axios, updated .gitifnore file, added API route to server.js file.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+// Install axios
+const axios = require('axios');
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,10 +12,17 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+
+// Google Book Search
+app.get("/api/books", (req, res) => {
+  console.log("This route works");
+})
+
+
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
