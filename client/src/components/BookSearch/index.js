676f33f6a544775645127ef651bb340fe6536f6e
Santiago Enciso <whereispolaris@gmail.com>
2019-10-12 22:14:56 -0500

    Created BookSearch component.

---
+++ client/src/components/BookSearch/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BookSearch = () => {
+    return (
+        <form>
+            <label htmlFor="book">Book:</label>
+            <input type="text" name="book" id="book" />
+        </form>
+    )
+}
+
+export default BookSearch;
