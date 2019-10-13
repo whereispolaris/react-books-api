e43958c0d9fbe5974d25f4e53fe3fc986cdd3896
Santiago Enciso <whereispolaris@gmail.com>
2019-10-12 19:29:31 -0500

    Added saved books route and, morgan dependency, and express.json.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const axios = require('axios');
 const mongoose = require('mongoose');
+const logger = require("morgan");
 
 // db models
 const db = require('./models');
@@ -13,10 +14,17 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -34,6 +42,17 @@ app.post("/api/submit", (req, res) => {
     .catch(err => {
       res.json(err);
     });
+})
+
+// View All Saved Books
+app.get("/api/saved", (req, res) => {
+  db.Book.find({})
+    .then(dbArticle => {
+      res.json(dbArticle);
+    })
+    .catch(err => {
+      res.json(err);
+    })
 })
 
 app.get("*", function (req, res) {
