b6a2cb5d626b47abdb82d970c29b7d514d1c488c
Santiago Enciso <whereispolaris@gmail.com>
2019-10-12 22:39:56 -0500

    Uninstalled npm react-bootstrap, added bootstrap starter file to public/index.html, and created Nav component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,13 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
       </div>
     );
   }
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="container-fluid">
+            <ul className="nav">
+                <li className="nav-item">
+                    <a className="nav-link active" href="#">LOGO</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Search</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Saved</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
