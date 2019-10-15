f45679c1f470b6f1cc4481abe50765bd609da5ad
Santiago Enciso <whereispolaris@gmail.com>
2019-10-14 21:23:34 -0500

    Enlarged and justified jumbotron font.

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,10 +4,10 @@ import "./Jumbotron.css"
 
 const Jumbotron = () => {
     return (
-        <div className="jumbotron">
+        <div className="jumbotron text-center">
             <Switch>
-                <Route exact path="/" render={() => <h1 className="display-4">React Google Books</h1>} />
-                <Route exact path="/saved" render={() => <h1 className="display-4">Saved Books</h1>} />
+                <Route exact path="/" render={() => <h1 className="display-2">React Google Books</h1>} />
+                <Route exact path="/saved" render={() => <h1 className="display-2">Saved Books</h1>} />
             </Switch>
             <p className="lead">Search for any book title, author, or topic!</p>
         </div>
