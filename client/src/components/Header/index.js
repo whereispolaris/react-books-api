a9e4f2d7b6056419a1ce0f3dabe05a1a071246bc
Santiago Enciso <whereispolaris@gmail.com>
2019-10-16 09:20:32 -0500

    Removed navbar links and added them to Jumbotron.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,13 +10,6 @@ const Header = () => {
                 <li className="nav-item">
                     <Link className="nav-link" to={"/"}><img src={bookLogo} /></Link>
                 </li>
-                <li className="nav-item">
-                    {/* Search: onClick focus on Search Bar */}
-                    <Link className="nav-link link" to={"/"}>Search</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link link" to={"/saved"}>Saved</Link>
-                </li>
             </ul>
         </div>
     )
--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import "./Jumbotron.css"
 
 const Jumbotron = () => {
     return (
         <div className="jumbotron text-center">
             <Switch>
-                <Route exact path="/" render={() => <h1 className="display-2">React Google Books</h1>} />
-                <Route exact path="/saved" render={() => <h1 className="display-2">Saved Books</h1>} />
+                <Route exact path="/" render={() => [<h1 className="display-2">React Google Books</h1>, <Link to="/saved" className="btn">View Saved Books</Link>, <p className="lead">Search for any book title, author, or topic!</p>]} />
+                <Route exact path="/saved" render={() => [<h1 className="display-2">Saved Books</h1>, <Link to="/" className="btn"> Search again</Link>, <p className="lead">Manage Your Saved Books </p>]} />
             </Switch>
-            <p className="lead">Search for any book title, author, or topic!</p>
         </div>
     )
 }
